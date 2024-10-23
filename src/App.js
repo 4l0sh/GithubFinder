868135e0168d6c3a4407834d115c3381e969e343
ALi <2155399@roc-dev.com>
2024-10-23 08:54:19 +0200

    backupp

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import Navbar from './Components/Layout/Navbar'
+import './App.css';
+import Users from './Components/users/Users';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount() {
+    this.setState({loading: true});
+
+   const res = await axios.get('https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&CLIENT_SECRET=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}')
+    this.setState({users: res.data, loading: false});
+   console.log(res.data);
+
+  }
+
+  render() {
+
+
+     return (
+    <div className="App">
+      <Navbar title=" Github Finder" />
+      <div className='container'>
+        <Users loading={this.state.loading} users={this.state.users} />
+      </div>
+      
+    </div>
+  );
+  }
+ 
+}
+
+export default App;
