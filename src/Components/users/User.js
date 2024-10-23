f84553e269df52cd2476148d81e743be5844fee8
ALi <2155399@roc-dev.com>
2024-10-23 15:14:16 +0200

    user code

--- src/Components/users/User.js
+++ src/Components/users/User.js
@@ -1,36 +1,46 @@
-import React,
-{ Component } from 'react'
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-export class user extends Component {
+const User = ({ getUser, user, loading }) => {
+  const { login } = useParams(); // Get the login parameter from the URL
 
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login)
-  }
+  useEffect(() => {
+    getUser(login); // Call getUser when the component mounts
+  }, [getUser, login]); // Add getUser and login as dependencies
 
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable
-    } = this.props.user;
+  // Destructure user properties safely to handle loading or undefined user
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user || {};
 
-    const {loading} = this.props;
+  if (loading) return <div>Loading...</div>; // Show loading state
 
-    return (
-      <div>
-        {User}
-      </div>
-    )
-  }
-}
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={avatar_url} alt={name} />
+      <p>Location: {location}</p>
+      <p>Bio: {bio}</p>
+      <p>Blog: <a href={blog}>{blog}</a></p>
+      <p>Username: {login}</p>
+      <p>Followers: {followers}</p>
+      <p>Following: {following}</p>
+      <p>Public Repos: {public_repos}</p>
+      <p>Public Gists: {public_gists}</p>
+      <p>Hireable: {hireable ? 'Yes' : 'No'}</p>
+      <a href={html_url} target="_blank" rel="noopener noreferrer">View on GitHub</a>
+    </div>
+  );
+};
 
-export default user
+export default User;
